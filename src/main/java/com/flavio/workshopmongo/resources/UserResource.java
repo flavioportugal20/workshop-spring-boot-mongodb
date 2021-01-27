484317d2560972ac6a0fe29a4afc707ebf17d441
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-27 00:13:16 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/flavio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/flavio/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,10 @@ public class UserResource {
 		
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity <UserDTO> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
