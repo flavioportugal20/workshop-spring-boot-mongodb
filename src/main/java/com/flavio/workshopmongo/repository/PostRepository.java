c56f67c19285fcc03d127230373fbfe88784e0ee
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-28 18:10:17 -0300

    Consulta simples com @Query

--- src/main/java/com/flavio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/flavio/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.flavio.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.flavio.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.flavio.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
