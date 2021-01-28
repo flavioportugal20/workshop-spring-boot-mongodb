8152d0892068916717daafd9eafaf13e375cd371
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-28 09:53:30 -0300

    Consulta simples com query methods

---
+++ src/main/java/com/flavio/workshopmongo/resources/util/URL.java
@@ -0,0 +1,14 @@
+package com.flavio.workshopmongo.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class URL {
+	public static String decoeParam(String text) {
+		try {
+			return URLDecoder.decode(text,"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
