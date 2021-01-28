063eb839af67b4ed881954ad7e234346facee433
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-28 09:30:58 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/flavio/workshopmongo/domain/Post.java
+++ src/main/java/com/flavio/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.flavio.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.flavio.workshopmongo.dto.AuthorDTO;
+import com.flavio.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable{
@@ -16,6 +19,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	List<CommentDTO> comments = new ArrayList<>();
 
 	public Post(){
 	}
@@ -67,6 +72,14 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
