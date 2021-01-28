7db25f2afd4a3737cd932b09d95d380adfd33e2a
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-28 00:26:08 -0300

    Referenciando os posts do usuário

--- src/main/java/com/flavio/workshopmongo/config/Instantiation.java
+++ src/main/java/com/flavio/workshopmongo/config/Instantiation.java
@@ -43,6 +43,10 @@ public class Instantiation implements CommandLineRunner{
 				
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(maria);
+		
+		
 	}
 
 }
