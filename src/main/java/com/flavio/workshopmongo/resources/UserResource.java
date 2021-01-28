364cc4376c1b21ec9992c5d8021e228e5e551276
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-27 21:08:59 -0300

    Atualização de usuário com PUT

--- src/main/java/com/flavio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/flavio/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,15 @@ public class UserResource {
 	public ResponseEntity <UserDTO> delete(@PathVariable String id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity <UserDTO> insert(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
 	}
 
 }
