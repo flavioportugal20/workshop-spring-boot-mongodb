00da376652ad870c98f4c2892088bf4ebbfdf73d
flavioportugal20 <flavioportugal20@gmail.com>
2021-01-28 00:29:52 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/flavio/workshopmongo/resources/UserResource.java
+++ src/main/java/com/flavio/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.flavio.workshopmongo.domain.Post;
 import com.flavio.workshopmongo.domain.User;
 import com.flavio.workshopmongo.dto.UserDTO;
 import com.flavio.workshopmongo.services.UserService;
@@ -63,6 +64,12 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity <List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
